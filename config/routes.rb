e186ef1c07f1d944dce1d018f8c860f4d84761cb
Rory Walker <redrory@gmail.com>
2013-02-02 13:34:27 -0500

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Poutly::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

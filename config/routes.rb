8f00f355dcc2f411195242643481e4150920f626
Rory Walker <redrory@gmail.com>
2013-02-02 15:30:39 -0500

    Remove index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Poutly::Application.routes.draw do
+  root to: 'static_pages#home'
+
   get "static_pages/home"
 
-  get "static_pages/help"
+  match '/help', to: 'static_pages#help'
+
 
   get "static_pages/about"
+
+  get "static_pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

f3aed65a7a3b6f32a15fa67d1faf1e76ffc673bb
Rory Walker <redrory@gmail.com>
2013-02-02 13:16:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poutly::Application.config.secret_token = 'f2478d00a3f6e65b9acea96f9a207ae4a5758d38aceb6bf377b0316db205a96a8bc10e1bbdc35d3ba784d040e677249c6e12650e70b5b683793b0287c93fd4de'

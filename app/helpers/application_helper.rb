2310efcc165c154a711ac6696876174cf1c647be
Rory Walker <redrory@gmail.com>
2013-02-02 14:55:51 -0500

    Update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	#returns the full title on a per-page basis
+
+	def full_title(page_title)
+		base_title = "Poutly.com"
+
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title}| #{page_title}"
+		end
+		
+	end
 end

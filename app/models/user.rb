afcc7282eb568494befd4d53b6a2567cfb826a42
Rory Walker <redrory@gmail.com>
2013-02-03 13:08:15 -0500

    Finshed sign

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6}
 
   validates :password_confirmation, presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+      
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Poutly::Application.routes.draw do
   resources :users
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   root to: 'static_pages#home'
 
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy'
+
 
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'

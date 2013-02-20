efd545888fa69c1aed1be028ab1531db8fd6364a
Rory Walker <redrory@gmail.com>
2013-02-19 21:36:34 -0500

    Paid job removes from home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
        @client = current_user.clients.build
-       @feed_items = current_user.feed.paginate(page: params[:page])
+       #@feed_items = current_user.feed.paginate(page: params[:page])
+       @feed_items = current_user.feed.find_all_by_paid(false)
     end
    
   end

a1c06e29d20f054bdc46ee54700f4f0ff0bcd7c9
Rory Walker <redrory@gmail.com>
2013-02-22 22:14:30 -0500

    Due_date glich

--- db/migrate/20130222022801_change_date_column_to_date_time.rb
+++ db/migrate/20130222022801_change_date_column_to_date_time.rb
@@ -1,11 +1,11 @@
 class ChangeDateColumnToDateTime < ActiveRecord::Migration
   def up
-  	remove_column :clients, :due_date, :date
+  	remove_column :clients, :due_date, :string
   	add_column :clients, :due_date, :date
   end
 
   def down
-  	remove_column :clients, :due_date, :date
+  	remove_column :clients, :due_date, :string
   	add_column :clients, :due_date, :date
   end
 end

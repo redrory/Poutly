d8a94b7163cb61d94423dbaba91825937764577f
Rory Walker <redrory@gmail.com>
2013-05-07 21:34:05 -0500

    chaging values to keys

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['app15504772@heroku.com'],
-  :password       => ENV['ke6c2c8d'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }

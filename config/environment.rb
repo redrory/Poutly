0dd1e54c56eae2b46d51f7318696406761ddb685
Rory Walker <redrory@gmail.com>
2013-05-07 21:01:00 -0500

    Sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Poutly::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app15504772@heroku.com'],
+  :password       => ENV['ke6c2c8d'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}

43d80f062f725db1936db78fa5022c31393e4c7f
Rory Walker <redrory@gmail.com>
2013-06-09 18:27:26 -0500

    Basic refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,23 @@
 class User < ActiveRecord::Base
 
- attr_accessible :company, :email, :name, :password, :password_confirmation
- has_secure_password
- has_many :clients, dependent: :destroy
+attr_accessible :company, :email, :name, :password, :password_confirmation
+has_secure_password
+has_many :clients, dependent: :destroy
 
-  before_save {|user| user.email = email.downcase}
-  before_save :create_remember_token
-  before_create { generate_token(:auth_token) }
+before_save {|user| user.email = email.downcase}
+before_save :create_remember_token
+before_create { generate_token(:auth_token) }
 
-  validates :name, presence: true, length: { maximum: 35}
+validates :name, presence: true, length: { maximum: 35}
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 50}, format: {with: VALID_EMAIL_REGEX},
-  											uniqueness: {case_sensitive: false}
-  validates :company, presence: true, length: { maximum: 50}
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, presence: true, length: { maximum: 50}, format: {with: VALID_EMAIL_REGEX},
+											uniqueness: {case_sensitive: false}
+validates :company, presence: true, length: { maximum: 50}
 
-  validates :password, presence: true, length: { minimum: 6}
+validates :password, presence: true, length: { minimum: 6}
 
-  validates :password_confirmation, presence: true
+validates :password_confirmation, presence: true
 
   def send_password_reset
     generate_token(:password_reset_token)
